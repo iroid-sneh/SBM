e28e5044642a7c246cb604e0cf8d5c450b19393c
iroid-sneh <sneh@iroidsolutions.com>
2025-05-08 07:46:10 +0530

    first commit for sbm project

---
+++ public/assets/js/main.js
@@ -0,0 +1,5 @@
+
+// Placeholder for sidebar toggle and future interactions
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Dashboard loaded");
+});
---
+++ public/js/theme.js
@@ -0,0 +1,1 @@
+// JS for dark/light theme
