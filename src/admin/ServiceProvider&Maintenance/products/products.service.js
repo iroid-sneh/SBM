5e795576dda4ea6950cd52c3f5547882e3565416
Your GitHub Username <Your GitHub Email>
2025-05-26 15:43:17 +0530

    fix: Changes in the bussiness-details page in category selection.

--- src/admin/ServiceProvider&Maintenance/products/products.service.js
+++ src/admin/ServiceProvider&Maintenance/products/products.service.js
@@ -4,7 +4,61 @@ class productsServices {
      * @param {*} req
      * @param {*} res
      */
-    static async productsPage(req, res) {}
+    static async productsPage(req, res) {
+        // const user = req.user;
+
+        // const { preference, category } = user;
+
+        // const userType = `${preference}-${category}`;
+
+        const userType = "Service-Service Provider";
+
+        switch (userType) {
+            case "Service-Freelancer":
+                res.render(
+                    "adminPanels/serviceProvider&Maintenance/products/Service-Freelancer/serviceFreelancer",
+                    {
+                        currentPage: "products",
+                        layout: "adminPanels/serviceProvider&Maintenance/layouts/layout",
+                        // user,
+                    }
+                );
+                break;
+            case "Service-Service Provider":
+                res.render(
+                    "adminPanels/serviceProvider&Maintenance/products/Service-ServiceProvider/serviceProvider",
+                    {
+                        currentPage: "products",
+                        layout: "adminPanels/serviceProvider&Maintenance/layouts/layout",
+                        // user,
+                    }
+                );
+                break;
+            case "Maintenance-Contractor Based":
+                res.render(
+                    "adminPanels/serviceProvider&Maintenance/products/Maintenance-ContractorBased/maintenanceContractor",
+                    {
+                        currentPage: "products",
+                        layout: "adminPanels/serviceProvider&Maintenance/layouts/layout",
+                        // user,
+                    }
+                );
+                break;
+            case "Maintenance-Service Provider":
+                res.render(
+                    "adminPanels/serviceProvider&Maintenance/products/Maintenance-ServiceProvider/maintenanceServiceProvider",
+                    {
+                        currentPage: "products",
+                        layout: "adminPanels/serviceProvider&Maintenance/layouts/layout",
+                        // user,
+                    }
+                );
+                break;
+            default:
+                res.redirect("/spadmin/dashboard");
+                break;
+        }
+    }
 }
 
 export default productsServices;
